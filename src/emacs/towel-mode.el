8c65d6f6fa94611fdff8b8811b5ee3b44cbdfc70
Zihang Chen <chsc4698@gmail.com>
2015-11-08 20:05:53 -0500

    add towel-mode

---
+++ src/emacs/towel-mode.el
@@ -0,0 +1,14 @@
+(setq tm-keywords
+      '("if>=0" "if>0" "if<=0" "if<0" "if=0" "if~0" "ift" "iff" "ife" "ifne"
+	"match" "bind" "also" "then" "fun" "type" "export" "`" "\\."))
+
+(setq tm-keywords-regexp (regexp-opt tm-keywords 'words))
+
+(setq tm-font-lock-keywords
+      `((,tm-keywords-regexp . font-lock-keyword-face)))
+
+(define-derived-mode towel-mode fundamental-mode
+  "Towel mode"
+  (setq font-lock-defaults '((tm-font-lock-keywords))))
+
+(provide 'towel-mode)
