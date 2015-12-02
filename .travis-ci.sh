05c1de2f120cb7a07f5f74cb446737cd745b196a
Zihang Chen <chsc4698@gmail.com>
2015-12-02 15:33:42 -0500

    fix typo

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -4,7 +4,7 @@ echo "yes" | sudo add-apt-repository ppa:avsm/ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
 export OPAMYES=1
-opan init
+opam init
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
 sudo apt-get install rbenv
