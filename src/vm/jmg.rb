5ca39838852d2044bf294e440e1282257290a678
Zihang Chen <chsc4698@gmail.com>
2015-12-02 21:34:06 -0500

    fix branching insts

--- src/vm/jmg.rb
+++ src/vm/jmg.rb
@@ -50,17 +50,19 @@ end
 ncs = numerical_conditionals.join "\n\n"
 
 nonnumerical_conditionals = [
-  ['JT', 'OVAtom', 'Uint64.compare i Uint64.one = 0'],
-  ['JF', 'OVAtom', 'Uint64.compare i Uint64.zero = 0'],
+  ['JT', 'OVAtom', 'Uint64.zero'],
+  ['JF', 'OVAtom', 'Uint64.one'],
 ].map do |xs|
   jn, cons, cond = xs
 
   "| #{jn}(ArgLit(VUFixedInt(j)))
 | H#{jn}(ArgLit(VUFixedInt(j))) ->
   #{match}
-      #{cons}(i) -> if #{cond}
-      #{clause}
-    | _ -> failwith \"Unsupported data type.\"
+      #{cons}(i) -> if Uint64.compare i #{cond} = 0
+      (* Take JF as an example,
+         only fall through when i = true, i.e. only jump when i <> true. *)
+      then next_ip else j_
+    | _ -> j_
   in _ip"
 end
 nncs = nonnumerical_conditionals.join "\n\n"
--- tests/functional.t
+++ tests/functional.t
@@ -15,5 +15,5 @@ bind :dec-1 (1 -)
 then ([42 41] :dec-1` /map !println
       0 [42 41 40] -` /foldr !println)
 
-bind ?not-true fun` ~x, (~x true = ift true, false)
+bind ?not-true ,\ ~x, (~x ift true, false)
 then ([1 3 true false true] ?not-true` /filter !println)
